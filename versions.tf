5414b44c87c79a4e9a4ec4651f9e4cf0c8f33090
Manuel Nuila <manuel.nuila@cuemby.com>
2024-10-21 10:31:22 -0600

    add random provider

--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "civo/civo"
       version = "1.0.49"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.3"
+    }
   }
   required_version = "~> 1.5.7"
 }
