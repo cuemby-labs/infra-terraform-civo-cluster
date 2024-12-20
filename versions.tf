7d8e0b5d536c31edb8dabfb53b55c06b82f40409
Manuel Nuila <manuel.nuila@cuemby.com>
2024-12-20 10:12:24 -0600

    update provider

--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     civo = {
       source  = "civo/civo"
-      version = "1.0.49"
+      version = ">= 1.0.49"
     }
     random = {
       source = "hashicorp/random"
       version = ">= 3.0.0"
     }
   }
-  required_version = "~> 1.5.7"
+  required_version = ">= 1.5.7"
 }
